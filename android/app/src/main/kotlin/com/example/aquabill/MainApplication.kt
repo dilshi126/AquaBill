a1e4cd864d00eab5b967bf43d0b0b29000b5b50c
Dilshi Wijesinghe <99238515+dilshi126@users.noreply.github.com>
2024-10-25 21:44:57 +0530

    add some changes to the files

---
+++ android/app/src/main/kotlin/com/example/aquabill/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.example.aquabill // Ensure this matches your package name
+
+import io.flutter.app.FlutterApplication
+
+class MainApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        // Custom initialization code can go here if needed
+    }
+}
